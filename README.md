3d6f4cadc281d92e5905733d77c9e8e50f092cf0
Erik Brinkman <erik.brinkman@gmail.com>
2015-12-30 13:13:29 -0600

    More progress

--- README.md
+++ README.md
@@ -1,85 +1,30 @@
 Postly
 ======
 
-An HTML / CSS / Javascript template for creating academic posters
+A webapp for academic poster editing
 
-Important
----------
-
-There is still one fairly large flaw. For some reason chrome (on ubuntu) is
-still scaling a page with absolute dimensions. In order to get the poster to
-actually fit on a page I had to manually tweak the zoom. You can do this by
-adding
-
-```css
-@media print {
-    html, body {
-        zoom: 1.15; /* or w/e is necessary */
-    }
-}
-```
-
-to your personal style sheet. This should allow you to print normally.
-
-Also, currently pdfs are supported by rendering on canvas. To get full vector
-support, it's recommended that you convert pdfs to svg before inclusion.
-
-Get Started
------------
-
-Everything you need to create a beautiful academic poster is set up for you in
-the root of this directory. Postly is developed in and for chrome, so ymmv in
-other browsers. I've included all of the dependencies here so that postly can
-be used offline.
-
-The most basic poster, with minimum styling is set up in
-[`index.html`](index.html). Modify this to add content to your poster. This
-used the [`basic.css`](basic.css) stylesheet to provide the necessary parameters
-for the poster to render. [`wolverine.css`](wolverine.css) is a more advanced
-stylesheet that was used to create [this demo](demos/wolverine/index.html).
-
-The template itself is fairly basic html, and should be easily understood with
-cursory knowledge of html. Similarly there's nothing too advanced with custom
-styling. Cursory knowledge of css and a look at `wolverine.css` should be
-enough to modify styling on your own. The main gotcha is that some items are
-necessarily sized to `100%`, and so to add a margin you may actually have to add
-padding to a parent, as well as make sure the `box-sizing` is `border-box`.
-
-To aid in development, there's a gulpfile that will set up browser
-syncing. Simply run `npm install` from the root directory, and then when you
-want to develop, run `gulp` (you may need to also install gulp globally). This
-will open up index.html in a new window, and refresh as soon as any relevant
-files are changed.
-
-Better documentation on how to actually tweak styles and add content should be
-here eventually. Until then, you can look at the demos for an example of how to
-add content as well as how to style it.
-
-Known Bugs / TODOs
-------------------
+TODOs
+-----
 
 * Make title editable, and have it reflect file saved to, and load it based off
   of file loaded.
 * Sync CSS
-* Drag and drop
-
-* Pdfs are rendered on canvas, as such, they lose their awesome vector
-  nature. Should replace this with some library that can load a pdf as an
-  svg. This may be possible with an svg backend of pdf.js.
-* Currently, the page is kept sized via javascript modifying the `html` `zoom`
-  property. Once the @viewport tag is supported in major browsers, that may
-  make a much better solution.
-* Due to zoom based solution, zooming in is impossible.
-* I may want to experiment with making the columns part of `flex-box` wrapping.
-* Improved Documentation.
-* At some point create a webapp that makes it easy to modify this with a
-  gui. This will likely embed the actual page as an iframe and go from there.
+* Insert images / edit images
+* CSS Themes
+* Webfonts
+* Edit title images
+* Certainly issues with hard coded editing colors  and some ui placement that
+  should at least get an adjustment for light or dark backgrounds or when
+  certain formatting is changed.
+* Use Ace or CodeMirror for better editing
+* MathJax fonts may not be saved...
+* Update licenses
+* Zoom on poster (unsure how to do this properly)
 
 Supporting Projects
 -------------------
 
 See the [licenses](licenses) folder
 
-* Normalize.css for a set of good defaults
+* Showdown for rendering markdown
 * MathJax for rendering equations
-* Pdf.js for pdf rendering.
--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 "use strict";
 window.addEventListener("load", () => {
-    document.getElementById("poster").contentWindow.addEventListener("load", () => {
+    document.querySelector("#poster").contentWindow.addEventListener("load", () => {
 
-        var pdoc = document.getElementById("poster").contentDocument;
+        // Poster Document
+        var pdoc = document.querySelector("#poster").contentDocument;
 
         // Close drawer when icon is clicked
-        document.getElementById("close-drawer")
+        document.querySelector("#close-drawer")
             .addEventListener("click", e => document.querySelector(".mdl-layout__drawer").classList.remove("is-visible"));
 
         // New poster 
@@ -15,16 +16,16 @@ window.addEventListener("load", () => {
             var loadNew = new XMLHttpRequest();
             loadNew.open('GET', 'new.html');
             loadNew.addEventListener('loadend', () => {
-                // TODO rewrite newPoster here
                 loadPoster(loadNew.responseText, 'postly');
             });
             loadNew.send();
         }
 
         // New poster button
-        document.getElementById("new").addEventListener("click", e => {
-            // TODO Prompt about loss of work
-            newPoster();
+        document.querySelector("#new").addEventListener("click", e => {
+            if (confirm("Do you want to delete the current poster?")) {
+                newPoster();
+            }
         });
 
         // Handle actual file load
@@ -34,30 +35,24 @@ window.addEventListener("load", () => {
 
             // Add global instrumentation
             var firstHead = nodes.head.childNodes[0];
-            var iconFont = nodes.createElement("link");
+            var iconFont = document.createElement("link");
             iconFont.classList.add("postly--instrumentation");
             iconFont.href = "//fonts.googleapis.com/icon?family=Material+Icons";
             iconFont.rel = "stylesheet";
             nodes.head.insertBefore(iconFont, firstHead);
-            var instrumentationStyle = nodes.createElement("link");
+            var instrumentationStyle = document.createElement("link");
             instrumentationStyle.classList.add("postly--instrumentation");
             instrumentationStyle.href = "instrumentation.css";
             instrumentationStyle.rel = "stylesheet";
             nodes.head.insertBefore(instrumentationStyle, firstHead);
-            var showdown = nodes.createElement("script");
-            showdown.classList.add("postly--instrumentation");
-            showdown.src = "//cdnjs.cloudflare.com/ajax/libs/showdown/1.3.0/showdown.min.js";
-            nodes.head.insertBefore(showdown, firstHead);
 
             // TODO There may be a more efficient way to move the objects
             // rather than converting to and from a string
             pdoc.documentElement.innerHTML = nodes.documentElement.innerHTML;
 
+            // Add title and author instrumentation
+
             // Add element level instrumentation
-            var columns = pdoc.querySelectorAll(".postly--column");
-            for (var i = 0; i < columns.length; i++) {
-                addColumnInstrumentation(columns[i]);
-            }
             var boxes = pdoc.querySelectorAll(".postly--box");
             for (var i = 0; i < boxes.length; i++) {
                 addBoxInstrumentation(boxes[i]);
@@ -88,7 +83,7 @@ window.addEventListener("load", () => {
         document.getElementById("save").addEventListener("click", e => {
             var clone = pdoc.documentElement.cloneNode(true);
             // FIXME this doesn't remove everything
-            var instrumentations = clone.getElementsByClassName("postly--instrumentation");
+            var instrumentations = clone.querySelectorAll(".postly--instrumentation");
             for (var i = 0; i < instrumentations.length; i++) {
                 instrumentations[i].parentNode.removeChild(instrumentations[i]);
             }
@@ -110,90 +105,244 @@ window.addEventListener("load", () => {
         // Print button
         document.getElementById("print").addEventListener("click", e => document.getElementById("poster").contentWindow.print());
 
-        //-------------------
-        // Dragging Handlers 
-        //-------------------
-        var draggingBox = null;
+        // About button
+        document.querySelector("#about").addEventListener("click", e => open("//github.com/erikbrinkman/postly"));
+
+        /*-------------------
+         * Dragging Handlers
+         *-------------------
+         *
+         * Due to some idiosyncrasies with chrome, this takes the dragged div,
+         * elevates it to z = 2, then places overlays at z = 1 for all of the
+         * columns. A copy of the div is made which moves around to indicate
+         * the new position, or moved if within the same column.
+         */
+
+        var draggingBox = null,
+            dummyBox = null;
 
         function dragBoxStart(evt) {
-            // XXX A little fragile
-            var box = evt.target.parentNode.parentNode;
-            draggingBox = box;
-            evt.dataTransfer.setData("text/html", box.outerHTML);
+            draggingBox = evt.target.parentNode.parentNode.parentNode;
+            dummyBox = draggingBox.cloneNode(true);
+
+            draggingBox.classList.add("postly--dragging");
+            dummyBox.classList.add("postly--dummy-box");
+            
+            evt.dataTransfer.setData("text/html", draggingBox.outerHTML);
             evt.dataTransfer.effectAllowed = "move";
-            evt.dataTransfer.setDragImage(box, 0, 0);
-            box.classList.add("dragging");
+            evt.dataTransfer.setDragImage(draggingBox, 0, 0);
+
+            draggingBox.addEventListener("dragenter", dragBoxEnter);
+            var columns = pdoc.getElementsByClassName("postly--column");
+            for (var i = 0; i < columns.length; i++) {
+                var overlay = document.createElement("div");
+                overlay.classList.add("postly--column-overlay");
+                columns[i].insertBefore(overlay, columns[i].childNodes[0]);
+
+                overlay.addEventListener("dragenter", dragColumnOverlayEnter);
+                overlay.addEventListener("dragover", dragColumnOverlayOver);
+            }
         }
 
         function dragBoxEnd(evt) {
-            var box = draggingBox;
-            box.classList.remove("dragging");
-            draggingBox = null;
-        }
+            var newColumn = dummyBox.parentNode;
+            if (newColumn) {
+                newColumn.insertBefore(draggingBox, dummyBox);
+                newColumn.removeChild(dummyBox);
+            }
+            draggingBox.removeEventListener("dragenter", dragBoxEnter);
+            draggingBox.classList.remove("postly--dragging", "postly--dummy-box");
 
-        function dragColumnEnter(evt) {
-            if (evt.target.classList.contains("postly--column")) {
-                console.log("enter", evt);
+            var overlays = pdoc.querySelectorAll(".postly--column-overlay");
+            for (var i = 0; i < overlays.length; i++) {
+                overlays[i].parentNode.removeChild(overlays[i]);
             }
         }
 
-        function dragColumnLeave(evt) {
-            if (evt.target.classList.contains("postly--column")) {
-                console.log("leave", evt); 
+        function dragColumnOverlayEnter(evt) {
+            var column = evt.target.parentNode;
+            if (column === draggingBox.parentNode) {
+                if (dummyBox.parentNode) {
+                    dummyBox.parentNode.removeChild(dummyBox);
+                }
+                draggingBox.classList.add("postly--dummy-box");
+            } else {
+                draggingBox.classList.remove("postly--dummy-box");
+                column.appendChild(dummyBox);
             }
         }
 
-        function dragColumnOver(evt) {
-
+        function dragBoxEnter(evt) {
+            if (dummyBox.parentNode) {
+                dummyBox.parentNode.removeChild(dummyBox);
+            }
+            draggingBox.classList.add("postly--dummy-box");
         }
 
-        //--------------------
-        // Add instrumentation
-        //---------------------
-        function addColumnInstrumentation(column) {
-            column.addEventListener("dragenter", dragColumnEnter);
-            column.addEventListener("dragover", dragColumnOver);
-            column.addEventListener("dragleave", dragColumnLeave);
+        function dragColumnOverlayOver(evt) {
+            var column = evt.target.parentNode,
+                divs = column.childNodes,
+                currentBeforeOrAfter = -1,
+                dummyDiv = null,
+                closestDiv = null,
+                closestDist = Infinity,
+                closestBeforeOrAfter = -1;
+            for (var i = 0; i < divs.length; i++) {
+                if (divs[i].nodeType == 1 && divs[i].classList.contains("postly--box")) {
+                    var rect = divs[i].getBoundingClientRect();
+                    var dist = Math.min(Math.abs(rect.top - evt.clientY), Math.abs(rect.bottom - evt.clientY));
+                    if (divs[i].classList.contains("postly--dummy-box")) {
+                        currentBeforeOrAfter = 0;
+                        dummyDiv = divs[i];
+                    } else if (currentBeforeOrAfter === 0) {
+                        currentBeforeOrAfter = 1;
+                    }
+
+                    if (dist < closestDist) {
+                        closestDist = dist;
+                        closestDiv = divs[i];
+                        closestBeforeOrAfter = currentBeforeOrAfter;
+                    }
+                }
+            }
+            if (closestBeforeOrAfter > 0) {
+                column.insertBefore(dummyDiv, closestDiv.nextSibling);
+            } else if (closestBeforeOrAfter < 0) {
+                column.insertBefore(dummyDiv, closestDiv);
+            }
+        }
+
+        /*----------------------------
+         * Instrumentation for Editing
+         *-----------------------------
+         */
+
+        var converter = new showdown.Converter({
+            noHeaderId: true,
+            headerLevelStart: 4,
+            tables: true
+        });
+
+        function renderTitleSource(source) {
+            var render = source.parentNode.querySelector(".postly--box-title");
+            source.setAttribute("value", source.value);
+            render.innerHTML = "";
+            render.appendChild(document.createTextNode(source.value));
+            MathJax.Hub.Queue(["Typeset", MathJax.Hub, render]);
+            source.style.display = "none";
+        }
+
+        function renderSource(source) {
+            var value = source.value;
+            // TODO Intercept images here to load
+            var render = source.parentNode.querySelector(".postly--box-content-render");
+            source.innerHTML = "";
+            source.appendChild(document.createTextNode(value));
+            render.innerHTML = converter.makeHtml(value);
+            MathJax.Hub.Queue(["Typeset", MathJax.Hub, render]);
+            source.style.display = "none";
         }
 
         // Function that adds instrumentation to a box
         function addBoxInstrumentation(box) {
-            var dragHandle = pdoc.createElement("i");
-            dragHandle.classList.add("material-icons", "postly--instrumentation", "postly--drag-handle");
+            var interactionBox = document.createElement("div");
+            interactionBox.classList.add("postly--box-interaction", "postly--instrumentation");
+
+            var spacer = document.createElement("div");
+            interactionBox.appendChild(spacer);
+            spacer.classList.add("postly--box-interaction-spacer");
+
+            var dragHandle = document.createElement("i");
+            interactionBox.appendChild(dragHandle);
+            dragHandle.classList.add("material-icons", "postly--drag-handle");
             dragHandle.draggable = true;
             dragHandle.addEventListener("dragstart", dragBoxStart);
             dragHandle.addEventListener("dragend", dragBoxEnd);
-            dragHandle.appendChild(pdoc.createTextNode("drag_handle"));
+            dragHandle.appendChild(document.createTextNode("drag_handle"));
 
-            var deleteHandle = pdoc.createElement("i");
-            deleteHandle.classList.add("material-icons", "postly--instrumentation", "postly--delete-handle");
-            deleteHandle.addEventListener("click", e => box.parentNode.removeChild(box));
-            deleteHandle.appendChild(pdoc.createTextNode("delete"));
+            var titleSource = box.querySelector(".postly--box-title-source");
+            titleSource.addEventListener("focusout", e => renderTitleSource(titleSource));
+            titleSource.addEventListener("keypress", e => {
+                if (e.keyIdentifier === "Enter") {
+                    titleSource.blur();
+                }
+            });
+            renderTitleSource(titleSource);
+
+            var editHandle = document.createElement("i");
+            interactionBox.appendChild(editHandle);
+            editHandle.classList.add("material-icons", "postly--edit-handle");
+            editHandle.addEventListener("click", e => {
+                titleSource.style.display = "block";
+                titleSource.focus();
+            });
+            editHandle.appendChild(document.createTextNode("mode_edit"));
+
+            var deleteHandle = document.createElement("i");
+            interactionBox.appendChild(deleteHandle);
+            deleteHandle.classList.add("material-icons", "postly--delete-handle");
+            deleteHandle.addEventListener("click", e => {
+                if (confirm("Do you wish to delete this box?")) {
+                    box.parentNode.removeChild(box);
+                }
+            });
+            deleteHandle.appendChild(document.createTextNode("delete"));
 
             var headerDiv = box.querySelector(".postly--box-header");
-            var headerTitle = headerDiv.childNodes[0];
-            headerDiv.insertBefore(dragHandle, headerTitle);
-            headerDiv.insertBefore(deleteHandle, headerTitle);
+            headerDiv.insertBefore(interactionBox, headerDiv.childNodes[0]);
+
+            var source = box.querySelector(".postly--box-content-source");
+            source.addEventListener("keypress", e => {
+                if (e.keyIdentifier === "Enter" && e.shiftKey) {
+                    e.preventDefault();
+                    source.blur(); // Causes focus out render, prevents double render
+                }
+            });
+            source.addEventListener("focusout", e => renderSource(source));
+            renderSource(source);
+
+            var contentEditOverlay = document.createElement("div");
+            contentEditOverlay.classList.add("postly--content-edit-overlay", "postly--instrumentation");
+            contentEditOverlay.addEventListener("click", e => {
+                source.style.display = "block";
+                source.focus();
+            });
+            var edit = document.createElement("i");
+            contentEditOverlay.appendChild(edit);
+            edit.classList.add("material-icons", "postly--content-edit-icon");
+            edit.appendChild(document.createTextNode("mode_edit"));
+            var content = box.querySelector(".postly--box-content");
+            content.insertBefore(contentEditOverlay, content.childNodes[0]);
         }
 
         // Add event listener for "add box" button
-        document.getElementById("add-element").addEventListener("click", e => {
-            var box = pdoc.createElement("div");
+        document.querySelector("#add-element").addEventListener("click", e => {
+            var box = document.createElement("div");
             box.classList.add("postly--box");
 
-            var headerDiv = pdoc.createElement("div");
+            var headerDiv = document.createElement("div");
             box.appendChild(headerDiv);
             headerDiv.classList.add("postly--box-header");
 
-            var header = pdoc.createElement("h3");
+            var headerSource = document.createElement("input");
+            headerDiv.appendChild(headerSource);
+            headerSource.classList.add("postly--box-title-source");
+            headerSource.value = "Click to add box title";
+            var header = document.createElement("h3");
             headerDiv.appendChild(header);
             header.classList.add("postly--box-title");
-            header.appendChild(pdoc.createTextNode("Box Title"));
+            header.appendChild(document.createTextNode("Box Title"));
 
-            var content = pdoc.createElement("div");
+            var content = document.createElement("div");
             box.appendChild(content);
             content.classList.add("postly--box-content");
-            content.appendChild(pdoc.createTextNode("Box Content"));
+            var source = document.createElement("textarea");
+            content.appendChild(source);
+            source.classList.add("postly--box-content-source");
+            source.appendChild(document.createTextNode("Click to edit box markdown"));
+            var render = document.createElement("div");
+            content.appendChild(render);
+            render.classList.add("postly--box-content-render");
 
             // Add instrumentation
             addBoxInstrumentation(box);
